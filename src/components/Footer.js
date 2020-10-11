ef3a9e9451dfc1751752ec693201a0a774071f9c
cyantis <cyantis@gmail.com>
2020-10-11 14:58:22 -0600

    adds footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../stylesheets/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <p>Hudson, New York | Arvada, Colorado</p>
+    </div>
+  )
+}
+
+export default Footer
