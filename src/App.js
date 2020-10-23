e9229cbb304c12d71e4622a63ce577aa2502b224
Cody Yantis <cody_yantis@skookum.com>
2020-10-23 15:53:46 -0600

    updates imports & adds components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './stylesheets/App.css'
-import Section from './components/Section.js'
+import Bio from './components/Bio.js'
 import Footer from './components/Footer.js'
-import { bio, player } from './assets/Text.js'
+import Discography from './components/Discography'
 
 const App = () => {
   return (
     <div className='App'>
       <h1>Tilth</h1>
-      <Section text={bio} id={'bio'} player={player} />
+      <Bio id={'bio'} />
+      <Discography id={'discography'} />
       <Footer />
     </div>
   )
