b6dd3d50955aa6bbb8bdf7e8aad02a78f0bb9049
Cody Yantis <cody_yantis@skookum.com>
2020-11-03 12:24:51 -0700

    moves contact link to header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,6 @@ const Footer = () => {
   return (
     <div className='Footer'>
       <p>Hudson, New York | Arvada, Colorado</p>
-      <p>
-        <a href='mailto:tilthtrio@gmail.com'>Contact</a>
-      </p>
     </div>
   )
 }
