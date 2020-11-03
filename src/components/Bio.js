9560160409efccf7e3a8a0da790466e1eac932e3
Cody Yantis <cody_yantis@skookum.com>
2020-11-03 12:24:32 -0700

    updates bio text

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,14 +7,9 @@ const Bio = (props) => {
       <p>
         Tilth are{' '}
         <a href='https://www.nathanmclaughlin.zone/'>Nathan McLaughlin</a> and{' '}
-        <a href='http://codyyantis.com'>Cody Yantis</a>, with contributions from
-        friends like{' '}
-        <a href='https://www.discogs.com/artist/1632484-Joe-Houpert'>
-          Joe Houpert
-        </a>
-        . Rooted in environment and place and utilizing strings, tape, horns,
-        keys, and electronics, Tilth aims to offer purposeful, intentional
-        music.
+        <a href='http://codyyantis.com'>Cody Yantis</a>. Rooted in environment
+        and place and utilizing strings, tape, horns, keys, and electronics,
+        Tilth aims to offer purposeful, intentional music.
       </p>
     </div>
   )
